4fdaa72b3b9c4f5328ef636677b9e8cd921c3ac9
rickdavidson <rickinaction@gmail.com>
2024-09-02 17:42:52 +1000

    22_RB_UC3D4 Respond To Debug Keys

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
@@ -13,15 +14,33 @@ public class CollisionHandler : MonoBehaviour
     AudioSource audioSource;
 
     bool isControllable = true;
+    bool isCollidable = true;
 
     private void Start() 
     {
         audioSource = GetComponent<AudioSource>();
     }
     
+    private void Update() 
+    {
+        RespondToDebugKeys();        
+    }
+
+    void RespondToDebugKeys()
+    {
+        if (Keyboard.current.lKey.wasPressedThisFrame)
+        {
+            LoadNextLevel();
+        }
+        else if (Keyboard.current.cKey.wasPressedThisFrame)
+        {
+            isCollidable = !isCollidable;
+        }
+    }
+
     private void OnCollisionEnter(Collision other) 
     {
-        if (!isControllable) { return; }
+        if (!isControllable || !isCollidable) { return; }
         
         switch (other.gameObject.tag)
         {
